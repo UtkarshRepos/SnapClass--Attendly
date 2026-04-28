bb245c21a2f1ac8834628704d170c58852c6586a
Utkarsh Chaudhary <choudharyutkarsh221@gmail.com>
2026-04-28 17:16:38 +0530

    Final commit

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -7,7 +7,7 @@ import io
 @st.dialog('Share Class Link')
 def share_subject_dialog(subject_name, subject_code):
 
-    app_domain = "snapclass-main.streamlit.app"  # Replace with your actual domain
+    app_domain = "attendly-main.streamlit.app"  # Replace with your actual domain
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join Class")
